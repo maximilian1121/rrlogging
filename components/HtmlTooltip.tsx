8b731e09ee1364503a954d705b811c148d79efb4
Maximilian <latificlynch@gmail.com>
2026-01-31 11:35:53 -0700

    refactor: remove unused Theme import and adjust tooltip font size

--- components/HtmlTooltip.tsx
+++ components/HtmlTooltip.tsx
@@ -1,16 +1,15 @@
 import styled from "@emotion/styled";
-import { Tooltip, tooltipClasses, TooltipProps, Theme } from "@mui/material";
+import { Tooltip, tooltipClasses, TooltipProps } from "@mui/material";
 
 const HtmlTooltip = styled(({ className, ...props }: TooltipProps) => (
   <Tooltip {...props} classes={{ popper: className }} />
-))(({ theme }: { theme: Theme }) => ({
+))(({ theme }) => ({
   [`& .${tooltipClasses.tooltip}`]: {
     backgroundColor: '#f5f5f9',
     color: 'rgba(0, 0, 0, 0.87)',
     maxWidth: 220,
-    fontSize: theme.typography.pxToRem(12),
     border: '1px solid #dadde9',
   },
 }));
 
-export default HtmlTooltip
+export default HtmlTooltip;
